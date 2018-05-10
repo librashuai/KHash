022da91db7b3ec113f32226f865a0c44bf512f6f
kevinyin <yinpengshuai710@live.com>
2018-05-10 17:17:34 +0800

    File and String hash UI

--- KHash/MainWindow.xaml.cs
+++ KHash/MainWindow.xaml.cs
@@ -23,11 +23,41 @@ namespace KHash
         public MainWindow()
         {
             InitializeComponent();
+            this.LeftMenu.Visibility = Visibility.Hidden;
+            SwitchContent(new FileHashView());
         }
 
         private void WindowMove(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void MenuBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.LeftMenu.Visibility = this.LeftMenu.IsVisible?Visibility.Hidden:Visibility.Visible;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SwitchContent(UserControl content)
+        {
+            this.ContentContainer.Children.Clear();
+            this.ContentContainer.Children.Add(content);
+
+            this.LeftMenu.Visibility = Visibility.Hidden;
+        }
+
+        private void FileMenu_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchContent(new FileHashView());
+        }
+
+        private void StringMenu_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchContent(new StringHashView());
         }
     }
 }
